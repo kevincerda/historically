d385cdc18b4dc04cd4b25628d79f4815473e925d
Kevin <kevin@cerda.io>
2018-12-06 13:20:55 -0800

    Defined results item structure.

--- client/components/ResultsItem.jsx
+++ client/components/ResultsItem.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const ResultsItem = props => {
   return (
     <div className="results-item">
-      <p>Item</p>
+      <div id="location">{props.item.category2}</div>
+      <div id="date">{props.item.date}</div>
+      <div id="description">{props.item.description}</div>
+      <div id="granularity">{props.item.granularity}</div>
+      <div id="language">{props.item.language}</div>
     </div>
   );
 };
