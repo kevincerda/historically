09edb8506136384e1da13246bc7af9b5ae8bf1af
Kevin <kevin@cerda.io>
2018-12-05 20:34:15 -0800

    Import and render search component.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Search from './Search.jsx';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, World.</h1>
+        <Search />
       </div>
     );
   }
