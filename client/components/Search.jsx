7e397a7ad5e2cb20a320bb8f92b7cecd0d927012
Kevin <kevin@cerda.io>
2018-12-05 20:50:07 -0800

    Added basic skeleton for search form.

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    event.preventDefault();
+    console.log(this.state.value);
+  }
+
   render() {
     return (
       <div id="search">
-        <h1>Search</h1>
+        <form>
+          <label>Search</label>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}>Submit</button>
+        </form>
       </div>
     );
   }
