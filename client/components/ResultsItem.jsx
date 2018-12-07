37b9106c8075a94f96aa7adbd988216b0fc8ece4
Kevin <kevin@cerda.io>
2018-12-07 12:45:24 -0800

    Defined layout and styles for results item.

--- client/components/ResultsItem.jsx
+++ client/components/ResultsItem.jsx
@@ -3,16 +3,25 @@ import React from 'react';
 const ResultsItem = props => {
   let resultItems = props.data.map((item, index) => {
     return (
-      <div className="results-item" key={index}>
-        <div id="location">{item.category2}</div>
-        <div id="date">{item.date}</div>
-        <div id="description">{item.description}</div>
-        <div id="granularity">{item.granularity}</div>
-        <div id="language">{item.language}</div>
+      <div>
+        <div className="results-item results-item-wrapper" key={index}>
+          <div id="results-left-col">
+            <div id="location">{item.category2}</div>
+            <div id="date">({item.date})</div>
+          </div>
+          <div id="results-right-col">
+            <div id="description">{item.description}</div>
+          </div>
+        </div>
+        <div className="line-divider" />
       </div>
     );
   });
-  return <div id="results">{resultItems}</div>;
+  return (
+    <div className="column-3">
+      <div className="results">{resultItems}</div>
+    </div>
+  );
 };
 
 export default ResultsItem;
