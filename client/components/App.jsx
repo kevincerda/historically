0cd7ce746b14e2dea63cc7e66654d0b9996636d7
Kevin <kevin@cerda.io>
2018-12-07 12:44:53 -0800

    Added method to update state with search results data.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import Search from './Search.jsx';
+import NavBar from './NavBar.jsx';
+import ResultsItem from './ResultsItem.jsx';
+import Paginate from './Paginate.jsx';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchResults: ''
+    };
+    this.updateData = this.updateData.bind(this);
+  }
+
+  updateData(data) {
+    this.setState({ searchResults: data });
+  }
+
   render() {
     return (
-      <div>
-        <Search />
+      <div id="main-wrapper">
+        <NavBar updateData={this.updateData} />
+        <div id="results">
+          {this.state.searchResults ? (
+            <ResultsItem data={this.state.searchResults} />
+          ) : null}
+          {this.state.pageCount ? <Paginate /> : null}
+        </div>
       </div>
     );
   }
