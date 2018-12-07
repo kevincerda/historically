cf7e60e2cde5c13b80f6e1ff9419d05e1f66f308
Kevin <kevin@cerda.io>
2018-12-07 15:43:01 -0800

    Added methods to retrieve data back from child components.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,24 +7,62 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      searchResults: ''
+      searchQueryValue: '',
+      searchResults: [],
+      page: 1,
+      resultsCount: undefined,
+      pageCount: undefined
     };
     this.updateData = this.updateData.bind(this);
+    this.updateDataFromPageClick = this.updateDataFromPageClick.bind(this);
   }
 
-  updateData(data) {
-    this.setState({ searchResults: data });
+  updateData(dataObj) {
+    const {
+      searchQueryValue,
+      searchResults,
+      resultsCount,
+      pageCount
+    } = dataObj;
+    this.setState({
+      searchQueryValue: searchQueryValue,
+      searchResults: searchResults,
+      resultsCount: resultsCount,
+      pageCount: pageCount
+    });
+  }
+
+  updateDataFromPageClick(dataObj) {
+    const { searchResults, page } = dataObj;
+    this.setState({
+      searchResults: searchResults,
+      page: page
+    });
   }
 
   render() {
     return (
       <div id="main-wrapper">
-        <NavBar updateData={this.updateData} />
-        <div id="results">
+        <NavBar
+          updateData={this.updateData}
+          page={this.state.page}
+          resultsCount={this.state.resultsCount}
+          pageLinks={this.state.pageLinks}
+          pageCount={this.state.pageCount}
+        />
+        <div className="column-3" id="results">
           {this.state.searchResults ? (
             <ResultsItem data={this.state.searchResults} />
           ) : null}
-          {this.state.pageCount ? <Paginate /> : null}
+          {this.state.pageCount ? (
+            <Paginate
+              searchQueryValue={this.state.searchQueryValue}
+              searchResults={this.state.searchResults}
+              page={this.state.page}
+              pageCount={this.state.pageCount}
+              updateDataFromPageClick={this.updateDataFromPageClick}
+            />
+          ) : null}
         </div>
       </div>
     );
