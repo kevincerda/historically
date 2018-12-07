01476829982fed0aa0ac3394b6273285dfe483ad
Kevin <kevin@cerda.io>
2018-12-07 15:44:59 -0800

    Updated component to receive props, moved page click method from search component, wrapped reactpaginate in div with class for styling.

--- client/components/Paginate.jsx
+++ client/components/Paginate.jsx
@@ -1,31 +1,53 @@
 import React, { Component } from 'react';
 import ReactPaginate from 'react-paginate';
+import axios from 'axios';
 
 export default class Paginate extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      page: 1,
-      resultsCount: undefined,
-      pageLinks: undefined,
-      pageCount: undefined
+      searchQueryValue: props.searchQueryValue,
+      page: props.page,
+      pageCount: props.pageCount
     };
+    this.updateDataFromPageClick = this.props.updateDataFromPageClick.bind(
+      this
+    );
+    this.handlePageClick = this.handlePageClick.bind(this);
   }
+
+  handlePageClick(data) {
+    let page = data.selected + 1;
+    axios
+      .get(`/events?q=${this.state.searchQueryValue}&_page=${page}`)
+      .then(res => {
+        this.updateDataFromPageClick({
+          page: page,
+          searchResults: res.data
+        });
+      })
+      .catch(error => {
+        console.log('Error', error);
+      });
+  }
+
   render() {
     return (
-      <ReactPaginate
-        previousLabel={'<'}
-        nextLabel={'>'}
-        breakLabel={'...'}
-        breakClassName={'break-me'}
-        pageCount={this.state.pageCount}
-        marginPagesDisplayed={1}
-        pageRangeDisplayed={9}
-        onPageChange={this.handlePageClick}
-        containerClassName={'pagination'}
-        subContainerClassName={'pages pagination'}
-        activeClassName={'active'}
-      />
+      <div className="pagination">
+        <ReactPaginate
+          previousLabel={'<'}
+          nextLabel={'>'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={this.state.pageCount}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={8}
+          onPageChange={this.handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
+      </div>
     );
   }
 }
